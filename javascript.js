9816f97451d111fe6557562c7f09669c83b34ed2
mingmingsal <miguellorenzosalvador@gmail.com>
2024-01-30 12:21:30 +0800

    fixed recursion bug in getPlayerChoice, added getChoices helper function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+let playerSelection;
+let computerSelection;
 function getComputerChoice(){
     const choices = ["rock","paper","scissors"];
     return choices[Math.floor(Math.random()*choices.length)];
@@ -9,16 +11,49 @@ switch(input){
     case "rock":
     case "paper":
     case "scissors":
+        console.log(input);
         return input;
-        break;
     default:
-        getPlayerChoice();
+        
+        return getPlayerChoice();
+        return "invalid";
 }
 }
-function playRound(){
-getPlayerChoice();
-getComputerChoice();
+function playRound(playerSelection, computerSelection){
+    console.log(playerSelection);
+    if (playerSelection==computerSelection){
+        return ("Draw!");
+    }
+    else if (playerSelection=="rock"){
+        if (computerSelection=="scissors"){
+            return ("Player wins!");
+        }
+        else {
+            return ("Computer wins!");
+        }
+    }
+    else if (playerSelection=="paper"){
+        if (computerSelection=="rock"){
+            return ("Player wins!");
+        }
+        else {
+            return ("Computer wins!");
+        }
+    }
+    else if (playerSelection=="scissors"){
+        if (computerSelection=="paper"){
+            return ("Player wins!");
+        }
+        else {
+            return ("Computer wins!");
+        }
+    }
 }
 function playGame(){
 
+}
+function getChoices(){
+    playerSelection = getPlayerChoice();
+    computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
 }
