7bdf2c4859420fd337d94d077cc19a811a1792ac
mingmingsal <miguellorenzosalvador@gmail.com>
2024-01-30 11:53:05 +0800

    added player choice function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,22 @@
 function getComputerChoice(){
     
 }
+function getPlayerChoice(){
+
+let input = prompt("Play your hand", "Rock, Paper, Scissors").toLowerCase();
+switch(input){
+    case "rock":
+    case "paper":
+    case "scissors":
+        return input;
+        break;
+    default:
+        getPlayerChoice();
+}
+}
+function playRound(){
+
+}
+function playGame(){
+
+}
